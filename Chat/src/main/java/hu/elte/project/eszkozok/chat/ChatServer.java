88ad3707dca5573d349d4372fac05b4c5735e97c
Arianna Pintér <pinterari@inf.elte.hu>
2017-05-23 18:12:39 +0200

    Dokumentációs kommentek a ChatServer osztályhoz, kód refaktorálása

--- Chat/src/main/java/hu/elte/project/eszkozok/chat/ChatServer.java
+++ Chat/src/main/java/hu/elte/project/eszkozok/chat/ChatServer.java
@@ -21,41 +21,63 @@ import hu.elte.project.eszkozok.chat.entity.ChatGroup;
 import hu.elte.project.eszkozok.chat.entity.Message;
 import hu.elte.project.eszkozok.chat.entity.User;
 
+/**
+ * <h1>ChatServer</h1>
+ * A ChatServer a chat alkalmazás szerveroldali részét valósítja meg.
+ * Kliensek csatlakozhatnak hozzá, és a köztük történő kommunikációt bonyolítja le.
+ * 
+ * @author Baráth Zsófia
+ */
 public class ChatServer {
 
-	private final int port;
+	private static final int PORT = 8081;
 	private ServerSocket server;
 
 	private Map<String, PrintWriter> clients = new HashMap<String, PrintWriter>();
 	private Map<String, Integer> currentRoom = new HashMap<String, Integer>();
 
-	public int getPort() {
-		return port;
-	}
-
+	/**
+	 * A szerver konstruktora, ami létrehoz egy ServerSocketet a megadott portszámmal,
+	 * amin keresztül kapcsolódni tudnak hozzá a kliensek.
+	 * 
+	 * @param port A port, amin keresztül a kliensek kapcsolódnak.
+	 */
 	ChatServer(int port) {
-		this.port = port;
 		try {
 			server = new ServerSocket(port);
-			System.out.println("A chat server elindult.");
+			System.out.println("A szerver elindult.");
 		} catch (IOException e) {
-			System.err.println("Hiba a chat server inditasanal.");
+			System.err.println("Hiba történt a szerver indításánál.");
 			e.printStackTrace();
 		}
 	}
 
+	/**
+	 * A kliensek fogadásáért felelős függvény. Minden socketen keresztül csatlakozott
+	 * kliensnek létrehoz egy ClientHandler példányt, majd elindítja az így létrejött szálat. 
+	 */
 	public void handleClients() {
 		while (true) {
 			try {
 				new ClientHandler(server.accept()).start();
 			} catch (IOException e) {
-				System.err.println("Hiba a kliensek fogadasakor.");
+				System.err.println("Hiba történt a kliensek fogadásakor.");
 				e.printStackTrace();
 			}
 		}
 	}
 
-	// Bejelentkezése
+	/**
+	 * Bejelentkezéskor validáció után a kliensek listájához adja a felhasználót.
+	 * <p> 
+	 * Hamisat ad vissza, ha nem létezik felhasználó ilyen névvel,
+	 * vagy hibás az adott névhez tartozó jelszó.
+	 * 
+	 * @param user		A felhasználói objektum, amit hozzá szeretnénk adni.
+	 * @param password	A begépelt jelszó.
+	 * @param pw		A csatorna, amin keresztül kommunikálunk a felhasználóval.
+	 * @return			Sikerült-e bejelentkeznie a felhasználónak.
+	 */
 	private synchronized boolean addClient(User user, String password, PrintWriter pw) {
 		if (clients.get(user.getUserName()) != null) {
 			return false;
@@ -66,118 +88,171 @@ public class ChatServer {
 			if (PasswordEncryptor.verifyPassword(user, password)) {
 				clients.put(user.getUserName(), pw);
 				currentRoom.put(user.getUserName(), -1);
-				send(user.getUserName(), "bejelentkezett.", "-1");
+				send(user.getUserName(), "bejelentkezett.");
 				return true;
 			} else {
-				System.out.println("Hibas jelszo");
+				System.out.println("Hibás jelszó!");
 			}
 
 		} catch (Exception e) {
-			System.err.println("Hiba a kliensek fogadasakor.");
+			System.err.println("Hiba történt a kliens fogadásakor.");
 			e.printStackTrace();
 		}
 		return false;
 	}
 
-	private synchronized void removeClient(String name) {
-		clients.remove(name);
-		currentRoom.remove(name);
+	/**
+	 * Eltávolítja a bejelentkezettek és a szobában tartózkodók listájáról a felhasználót.
+	 * 
+	 * @param username	Annak a felhasználónak a neve, akit el szeretnénk távolítani.
+	 */
+	private synchronized void removeClient(String username) {
+		clients.remove(username);
+		currentRoom.remove(username);
 	}
 
-	// Altalanos kuldes, a kliens uzenetet tovabbitja a tobbi kliensnek, az
-	// uzeneteket elmenti az adatbazisba
-	private synchronized void send(String name, String message, String room) {
-		System.out.println(name + ": " + message);
-
+	/**
+	 * Általános célú üzenetküldés.
+	 * <p>
+	 * A szerver elküldi a klienseknek az adott felhasználó üzenetét,
+	 * a be- és kijelentkezésről tájékoztat.
+	 * 
+	 * @param username	A felhasználó neve, aki írta az üzenetet.
+	 * @param message	A továbbküldendő üzenet.
+	 * @param room		A szoba, amelyikben a felhasználó éppen van.
+	 */
+	private synchronized void send(String username, String message) {
 		for (String n : clients.keySet()) {
-			if (!n.equals(name)) {
-				clients.get(n).println(name + ": " + message);
+			if (!n.equals(username)) {
+				clients.get(n).println(username + ": " + message);
 			}
 		}
 	}
 
-	// Az adott group korabbi uzeneteit osszeszedi, alkuldi a megadott usernek
-	private synchronized void sendPrevMessages(Integer groupId, String name) {
+	/**
+	 * Összegyűjti az adott szoba korábbi üzeneteit, majd átküldi a megadott nevű felhasználónak.
+	 * 
+	 * @param groupId	A szoba azonosítója.
+	 * @param name		A felhasználó neve.
+	 */
+	private synchronized void sendPrevMessages(int groupId, String name) {
 		List<Message> messages = MessageDAO.getMessages(groupId);
 
 		clients.get(name).println("PREVMESSAGES");
 		for (Message message : messages) {
 			String username = UserDAO.getUser(message.getUserID()).getUserName();
-			String toSend = username + ": " + message.getMessage() + " : " + message.getDate();
+			String toSend = "[" + message.getDate() + "] " + username + ": " + message.getMessage();
 			clients.get(name).println(toSend);
 		}
 		clients.get(name).println("DONE");
 	}
 
-	// Kikeresi hogy melyik felhasznalok vannak eppen benne a groupban
-	private synchronized void updatePrevMessages(int groupId, String name, String message) {
+	/**
+	 * Elküldi a megadott azonosítójú szobában tartózkodóknak az adott üzenetet.
+	 * 
+	 * @param groupId	A szoba azonosítója.
+	 * @param username	A felhasználó neve.
+	 * @param message	A felhasználó üzenete.
+	 */
+	private synchronized void updatePrevMessages(int groupId, String username, String message) {
 		try {
-			createMessage(groupId, name, message);
-			// Uzenet elkuldese
+			createMessage(groupId, username, message);
+
 			for (String n : currentRoom.keySet()) {
 				if (currentRoom.get(n) == groupId) {
-					clients.get(n).println(name + ": " + message);
+					clients.get(n).println(username + ": " + message);
 				}
 			}
 
 		} catch (Exception e) {
-			System.err.println("Hiba az uzenetek updateje kozben.");
+			System.err.println("Hiba történt az üzenetek betöltése közben.");
 			e.printStackTrace();
 		}
 
 	}
 
-	// Elkuldi a kliensnek hogy kik vannak bejelentkezve (a fooldali listahoz)
-	private synchronized void sendSignedInUsers(String name) {
-		clients.get(name).println("USERS");
+	/**
+	 * Elküldi a kliensnek az épp bejelentkezett felhasználók listáját, az adott
+	 * felhasználó kivételével. 
+	 * 
+	 * @param username	A felhasználó neve. 
+	 */
+	private synchronized void sendSignedInUsers(String username) {
+		clients.get(username).println("USERS");
 		for (String n : clients.keySet()) {
-			if (!n.equals(name)) {
-				clients.get(name).println(n + " is signed in ");
+			if (!n.equals(username)) {
+				clients.get(username).println(n + " be van jelentkezve.");
 			}
 		}
-		clients.get(name).println("DONE");
+		clients.get(username).println("DONE");
 	}
 
-	// Az osszes felhasznalot kuldi el a kliensnek (uj felhasznalo felvetele a
-	// szobaba)
-	private synchronized int sendValidUsersForinvite(String name, int roomID) {
+	/**
+	 * Új szoba létrehozásakor elküldi a kliensnek azokat a felhasználókat, 
+	 * akik még nincsenek benne az adott azonosítójú szobában,
+	 * azon felhasználó kivételével, aki létrehozta a szobát.
+	 * 
+	 * @param username	A szobát létrehozó felhasználó neve. 
+	 * @param roomID	A szoba azonosítója.
+	 * @return			Az összegyűjtött felhasználók száma.
+	 */
+	private synchronized int sendValidUsersForinvite(String username, int roomID) {
 		List<User> users = UserDAO.getUsers();
 		Set<User> usersInGroup = ChatGroupDAO.getChatGroup(roomID).getUserSet();
 		int counter = 0;
 
-		clients.get(name).println("USERS");
+		clients.get(username).println("USERS");
 		for (User user : users) {
-			if (!user.getUserName().equals(name) && !usersInGroup.contains(user)) {
+			if (!user.getUserName().equals(username) && !usersInGroup.contains(user)) {
 				String toSend = user.getId() + ": " + user.getUserName();
-				clients.get(name).println(toSend);
+				clients.get(username).println(toSend);
 				counter++;
 			}
 		}
-		clients.get(name).println("DONE");
+		clients.get(username).println("DONE");
 		return counter;
 	}
 
-	// A kliens szamara elerheto szobakat kuldi at
+	/**
+	 * Elküldi a felhasználó számára az elérhető szobákat.
+	 * 
+	 * @param user	A felhasználó, akinek elküldjük a szobákat.
+	 */
 	private synchronized void sendChatrooms(User user) {
 		clients.get(user.getUserName()).println("CHATROOMS");
+		
 		Set<ChatGroup> rooms = user.getChatGroupSet();
 		for (ChatGroup room : rooms) {
 			String toSend = room.getId() + ": " + room.getName();
 			clients.get(user.getUserName()).println(toSend);
 		}
+		
 		clients.get(user.getUserName()).println("DONE");
 	}
 
-	// Szoba mentése a táblába
-	public synchronized void createRoom(String name, String username) {
+	/**
+	 * Új szobát hoz létre, majd hozzárendeli az adott felhasználót.
+	 * 
+	 * @param roomName	Az új chatszoba neve.	
+	 * @param username	Annak a felhasználónak a neve, aki létrehozta a szobát.
+	 */
+	public synchronized void createRoom(String roomName, String username) {
 		User user = UserDAO.getUser(username);
 
 		ChatGroup room = new ChatGroup();
-		room.setName(name);
+		room.setName(roomName);
 		ChatGroupDAO.saveChatGroup(room);
 		ChatGroupDAO.addUserToChatGroup(room, user);
 	}
 
+	/**
+	 * Létrehoz egy üzenet objektumot a szoba azonosítója, a felhasználó neve és az üzenet szövege alapján, 
+	 * hozzárendeli a jelenlegi időpontot, majd ezt elmenti az adatbázisba.
+	 * 
+	 * @param roomId		A szoba azonosítója.
+	 * @param username		A felhasználó neve.
+	 * @param messageText	Az üzenet szövege.
+	 */
 	public synchronized void createMessage(Integer roomId, String username, String messageText) {
 		Message message = new Message();
 
@@ -192,13 +267,19 @@ public class ChatServer {
 		MessageDAO.saveMessage(message);
 	}
 
-	// Felhasznalo meghivasa a szobaba
+	/**
+	 * Hozzárendeli az adott azonosítójú felhasználót az adott azonosítójú chatszobához.
+	 * 
+	 * @param roomId			A szoba azonosítója, amibe meghívjuk a felhasználót.
+	 * @param invitedUserId		A meghívott felhasználó azonosítja.
+	 */
 	private synchronized void inviteUser(int roomId, int invitedUserId) {
 		User user = UserDAO.getUser(invitedUserId);
 		ChatGroup room = ChatGroupDAO.getChatGroup(roomId);
 		ChatGroupDAO.addUserToChatGroup(room, user);
 	}
 
+	
 	class ClientHandler extends Thread {
 
 		PrintWriter pw;
@@ -323,7 +404,7 @@ public class ChatServer {
 				System.err.println("Kommunikacios problema egy kliensnel. Nev: " + name);
 			} finally {
 				if (name != null)
-					send(name, "kijelentkezett.", "-1");
+					send(name, "kijelentkezett.");
 				removeClient(name);
 				try {
 					pw.close();
@@ -334,8 +415,15 @@ public class ChatServer {
 		}
 	}
 
+	/**
+	 * A szerver main metódusa, ami létrehoz egy ChatServer példányt a megadott portszámmal,
+	 * majd amíg nem szűnik meg az objektum, a klienseket kezeli.
+	 * 
+	 * @param args			Parancssori argumentumok listája.
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
-		ChatServer server = new ChatServer(8081);
+		ChatServer server = new ChatServer(PORT);
 		if (server != null)
 			server.handleClients();
 	}
